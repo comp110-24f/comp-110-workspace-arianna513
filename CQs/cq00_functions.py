ac8c318438649c2602e7875b9e5bfcd4c99266a8
Arianna <acbah@ad.unc.edu>
2024-09-10 14:24:59 -0400

    Tea Party

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,18 @@
+"""My First Challenge Question!"""
+
+__author__ = "730739313"
+
+
+def mimic(message: str) -> str:
+    "Type A Message to Have it Mimicked Back to You!"
+    return message
+
+
+def main() -> None:
+    print(mimic(message=input("What is your message?")))
+    """Enables the Mimic Function to be Return A Message"""
+    return
+
+
+if __name__ == "__main__":
+    main()
